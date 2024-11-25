aca0370fbc245754552e87a5b9d237a7cdbbdb31
SardorDev12 <sardoribnfarhod@gmail.com>
2024-11-25 20:00:45 +0500

    article page

--- Apps/profiles/models.py
+++ Apps/profiles/models.py
@@ -2,9 +2,12 @@ from django.db import models
 from django.conf import settings
 
 class Profile(models.Model):
-    user_id = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, to_field="username")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, to_field='username')
+    first_name = models.CharField(max_length=100)
+    second_name = models.CharField(max_length=100)
+    parents_name = models.CharField(max_length=100, null=True, blank=True)
     bio = models.TextField(default="", max_length=300)
     profile_pic_url = models.ImageField(upload_to='profile_pics', null=True, blank=True)
 
     def __str__(self):
-        return f"Profile for {self.user_id.username}"
+        return f"Profile for {self.username}"
