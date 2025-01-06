ac8173d2e38535d97e019342b5fc908710641cf8
SardorDev12 <sardoribnfarhod@gmail.com>
2025-01-06 19:35:59 +0500

    comment username user id

--- Apps/comments/serializers.py
+++ Apps/comments/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
 from .models import Comment
+from django.conf import settings
+
 
 class CommentSerializer(serializers.ModelSerializer):
+    username = serializers.SerializerMethodField()
+
+    def get_username(self, obj):
+      return obj.user.username,
+
+
     class Meta:
         model = Comment
-        fields = "__all__"
+        fields = ('id','content', 'post', 'user', 'username')
