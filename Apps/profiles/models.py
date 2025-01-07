ad63f4c15b8ec2842df14b60529cf5651e2816ac
SardorDev12 <sardoribnfarhod@gmail.com>
2025-01-07 13:42:20 +0500

    db migrations

--- Apps/profiles/models.py
+++ Apps/profiles/models.py
@@ -2,12 +2,12 @@ from django.db import models
 from django.conf import settings
 
 class Profile(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, to_field='username')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=100)
     second_name = models.CharField(max_length=100)
     parents_name = models.CharField(max_length=100, null=True, blank=True)
-    bio = models.TextField(default="", max_length=300)
+    bio = models.TextField(max_length=300, null=True, blank=True)
     profile_pic_url = models.ImageField(upload_to='profile_pics', null=True, blank=True)
 
     def __str__(self):
-        return f"Profile for {self.username}"
+        return f"Profile for {self.user}"
